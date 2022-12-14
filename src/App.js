1fccd95972be17061dfc308ba15096d302785263
kadenyi <treykadenyi@gmail.com>
2022-12-15 00:45:41 +0300

    Add navigation path

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import LandingPage from './Components/Landing/LandingPage';
+import Navbar from './Components/Navigation/Navbar';
 
 const App = () => {
   return (
     <div className="App">
+      <Navbar />
       <LandingPage />
     </div>
   );
