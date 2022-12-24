4e498bd4e8e7b5b95dbf595076557893a4b72811
kadenyi <treykadenyi@gmail.com>
2022-12-24 10:54:27 +0300

    Add show more functionality for the projects

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,57 +1,121 @@
-import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import React, { useState } from "react";
 
 import "./projects.css";
 import project1 from "../../Assets/Images/doc.png";
 
 const Projects = () => {
-  const { id } = useParams();
+  const [more, setMore] = useState(false);
 
   // add event listener to the popup link to open the popup
-const openPopup = (e) => {
-  e.preventDefault();
-  document.querySelector(".project_popup").classList.add("active");
-}
+  // const openPopup = (e) => {
+  //   e.preventDefault();
+  //   document.querySelector(".project_popup").classList.add("active");
+  // };
 
-// close popup
-const closePopup = (e) => {
-  e.preventDefault();
-  document.querySelector(".project_popup").classList.remove("active");
-}
-  
-  
-  
+  // // close popup
+  // const closePopup = (e) => {
+  //   e.preventDefault();
+  //   document.querySelector(".project_popup").classList.remove("active");
+  // };
+
   // create an array of all the projects
   const projectsContainer = [
     {
       id: 1,
       title: "Doc Appointments",
-      description: "Doc Appointments is a web application that eases the process of booking doctors' appointments for patients by allowing them to book appointments online.",
-      popup_description: "DOC- Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      technologies: ["React", "Redux", "Ruby on Rails", "PostgreSQL", "Bootstrap"],
+      description:
+        "Doc Appointments is a web application that eases the process of booking doctors' appointments for patients by allowing them to book appointments online.",
+      technologies: [
+        "React",
+        "Redux",
+        "Ruby on Rails",
+        "PostgreSQL",
+        "Bootstrap",
+      ],
       image: project1,
-      
     },
     {
       id: 2,
       title: "Event",
-      description: "Event Appointments is a web application that eases the process of booking doctors' appointments for patients by allowing them to book appointments online.",
-      popup_description: "EVENTs EVENTS- Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      technologies: ["React", "Redux", "Ruby on Rails", "PostgreSQL", "Bootstrap"],
+      description:
+        "Event Appointments is a web application that eases the process of booking doctors' appointments for patients by allowing them to book appointments online.",
+      technologies: [
+        "React",
+        "Redux",
+        "Ruby on Rails",
+        "PostgreSQL",
+        "Bootstrap",
+      ],
+      image: project1,
+    },
+    {
+      id: 3,
+      title: "Cookbook",
+      description:
+        "Cookbook is a web application that eases the process of booking doctors' appointments for patients by allowing them to book appointments online.",
+      technologies: [
+        "React",
+        "Redux",
+        "Ruby on Rails",
+        "PostgreSQL",
+        "Bootstrap",
+      ],
+      image: project1,
+    },
+    {
+      id: 4,
+      title: "Conference",
+      description:
+        "Co Appointments is a web application that eases the process of booking doctors' appointments for patients by allowing them to book appointments online.",
+      technologies: [
+        "React",
+        "Redux",
+        "Ruby on Rails",
+        "PostgreSQL",
+        "Bootstrap",
+      ],
+      image: project1,
+    },
+    {
+      id: 5,
+      title: "Stadium",
+      description:
+        "Stadium is a web application that eases the process of booking doctors' appointments for patients by allowing them to book appointments online.",
+      technologies: [
+        "React",
+        "Redux",
+        "Ruby on Rails",
+        "PostgreSQL",
+        "Bootstrap",
+      ],
       image: project1,
     },
      {
-      id: 3,
-      title: "Cookbook",
-      description: "Cookbook is a web application that eases the process of booking doctors' appointments for patients by allowing them to book appointments online.",
-      popup_description: "Cookbook- Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      technologies: ["React", "Redux", "Ruby on Rails", "PostgreSQL", "Bootstrap"],
+      id: 6,
+      title: "Spa",
+      description:
+        "Spa is a web application that eases the process of booking doctors' appointments for patients by allowing them to book appointments online.",
+      technologies: [
+        "React",
+        "Redux",
+        "Ruby on Rails",
+        "PostgreSQL",
+        "Bootstrap",
+      ],
       image: project1,
-      
     },
   ];
-  
-  
+
+// toggle hidden projects
+  const toggleHiddenProjects = (e) => {
+    setMore(!more);
+    e.target.innerText = more ? "See More" : "See Less";
+    const hiddenProjects = document.querySelectorAll(".hidden_project");
+    hiddenProjects.forEach((project) => {
+      project.classList.toggle("hide");
+    });
+  }
+
   return (
     <section className="projects_sect" id="projects">
       <div className="project_start">
@@ -60,7 +124,8 @@ const closePopup = (e) => {
           {/* single project */}
           {projectsContainer.map((project) => {
             return (
-              <div className="project" key={project.id}>
+              <div className={project.id <= 3 ? "project" : "project hidden_project hide"}
+                key={project.id}>
                 <div className="card">
                   <div className="project_img">
                     <img src={project.image} alt={project.title} />
@@ -75,25 +140,33 @@ const closePopup = (e) => {
                     </div>
                     {/* see project */}
                     <div className="project_links">
-                      <a href="#" className="project_link" onClick={openPopup}>
-                        <span id={project.id} className="link_span">Source</span>
-                        <span id={project.id} className="link_span">Source</span>
+                      <a href="#" className="project_link">
+                        <span id={project.id} className="link_span">
+                          Source
+                        </span>
+                        <span id={project.id} className="link_span">
+                          Source
+                        </span>
                       </a>
-                        <a href="#" className="project_link" onClick={openPopup}>
-                        <span id={project.id} className="link_span">Live</span>
-                        <span id={project.id} className="link_span">Live</span>
+                      <a href="#" className="project_link">
+                        <span id={project.id} className="link_span">
+                          Live
+                        </span>
+                        <span id={project.id} className="link_span">
+                          Live
+                        </span>
                       </a>
                     </div>
-
-               
                   </div>
                 </div>
               </div>
             );
-          }
-          )}
-          
+          })}
         </div>
+      </div>
+      {/* see more */}
+      <div className="see_more">
+        <button className="more_btn" onClick={toggleHiddenProjects}>See More</button>
       </div>
     </section>
   );
@@ -114,9 +187,28 @@ export default Projects;
 
 
 
-     {/* popup */}
-                
-                    {/* {project.id === e.target.id ? (
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{
+  /* popup */
+}
+
+{
+  /* {project.id === e.target.id ? (
                       <div className="project_popup">
                         <div className="popup_content">
                           <div className="popup_close" onClick={closePopup}>
@@ -129,10 +221,11 @@ export default Projects;
                     ) : (
                         ""
                     )}
-                     */}
+                     */
+}
 
-               
-                    {/* <div className="project_popup">
+{
+  /* <div className="project_popup">
                       <div className="popup_content">
                         <div className="popup_close" onClick={closePopup}>
                           &times;
@@ -142,6 +235,5 @@ export default Projects;
                           {project.popup_description}
                         </p>
                       </div>
-                    </div> */}
-
-
+                    </div> */
+}
