87b621142954c05b6633e39d9c54f866991fca18
Tracey Kadenyi <treykadenyi@gmail.com>
2026-03-30 15:03:32 +0300

    convert to ts

---
+++ src/Components/Landing/LandingPage.tsx
@@ -0,0 +1,118 @@
+import React, { useEffect } from "react";
+import anime from "animejs/lib/anime.es.js";
+import { RxDoubleArrowDown } from "react-icons/rx";
+import "./landingpage.css";
+
+// Import images
+import twitter from "../../Assets/Images/contact_icons/twitter.png";
+import linkedin from "../../Assets/Images/contact_icons/linkedin.png";
+import github from "../../Assets/Images/contact_icons/github.png";
+import medium from "../../Assets/Images/contact_icons/medium.png";
+import angelist from "../../Assets/Images/contact_icons/angelist.png";
+
+// Define type for social link
+interface SocialLink {
+  href: string;
+  img: string;
+  alt: string;
+  label: string;
+}
+
+const LandingPage = () => {
+  // Create blocks animation
+  useEffect(() => {
+    const container = document.querySelector(
+      ".landing_container",
+    ) as HTMLElement | null;
+
+    if (container) {
+      for (let i = 0; i <= 100; i++) {
+        const blocks = document.createElement("div");
+        blocks.classList.add("block");
+        container.appendChild(blocks);
+      }
+    }
+  }, []);
+
+  // Animate blocks function with proper typing for anime.js
+  const animateBlocks = (): void => {
+    anime({
+      targets: ".block",
+      translateX: () => anime.random(-700, 700),
+      translateY: () => anime.random(-500, 500),
+      scale: () => anime.random(1, 5),
+      easing: "linear",
+      duration: 2000,
+      delay: anime.stagger(10),
+      complete: animateBlocks,
+    });
+  };
+
+  animateBlocks();
+
+  // Social links data - makes code more maintainable
+  const socialLinks: SocialLink[] = [
+    {
+      href: "https://www.linkedin.com/in/tracey-kadenyi/",
+      img: linkedin,
+      alt: "linkedin",
+      label: "LinkedIn",
+    },
+    {
+      href: "https://github.com/trace-kadenyi",
+      img: github,
+      alt: "github",
+      label: "GitHub",
+    },
+    {
+      href: "https://angel.co/u/tracey-kadenyi",
+      img: angelist,
+      alt: "angelist",
+      label: "AngelList",
+    },
+    {
+      href: "https://twitter.com/traci_k7",
+      img: twitter,
+      alt: "twitter",
+      label: "Twitter",
+    },
+    {
+      href: "https://medium.com/@tracekadenyi",
+      img: medium,
+      alt: "medium",
+      label: "Medium",
+    },
+  ];
+
+  return (
+    <section className="blocks-sect" id="home">
+      <div className="landing_container">
+        <h2 className="span1">TRACEY</h2>
+        <h2 className="span2">KADENYI</h2>
+        <p className="title">Full Stack Web Developer</p>
+
+        {/* Contact Icons */}
+        <div className="landing_contact_icons">
+          <ul>
+            {socialLinks.map((link) => (
+              <li key={link.label}>
+                <a href={link.href} target="_blank" rel="noreferrer">
+                  <img src={link.img} alt={link.alt} />
+                  <span>{link.label}</span>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      <button style={{ zIndex: 10, marginTop: "50px" }}>
+        <a href="#about">
+          <RxDoubleArrowDown className="down" />
+        </a>
+      </button>
+    </section>
+  );
+};
+
+export default LandingPage;
